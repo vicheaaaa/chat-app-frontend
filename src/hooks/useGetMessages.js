30150a2cc2d0cddae3f4a9be5b912414eb9fbbe3
vicheaaaa <123868063+vicheaaaa@users.noreply.github.com>
2024-09-16 14:19:58 +0700

    Update useGetMessages.js

--- src/hooks/useGetMessages.js
+++ src/hooks/useGetMessages.js
@@ -9,7 +9,7 @@ const useGetMessages = () => {
     const getMessages = async () => {
       setLoading(true);
       try {
-        const res = await fetch(`https://chat-app-tan-nine-10.vercel.app/api/messages/${selectedConversation._id}`);
+        const res = await fetch(`https://chat-app-backend-zhg0.onrender.com/api/messages/${selectedConversation._id}`);
         const data = await res.json();
         if (data.error) {
           throw new Error(error.message);

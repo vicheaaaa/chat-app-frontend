b270bab12de2d0f8b24f5c83d52bc5804af8f26a
vicheaaaa <123868063+vicheaaaa@users.noreply.github.com>
2024-09-16 14:21:20 +0700

    Update useSendMessage.js

--- src/hooks/useSendMessage.js
+++ src/hooks/useSendMessage.js
@@ -9,7 +9,7 @@ const useSendMessage = () => {
     setLoading(true);
     try {
       const res = await fetch(
-        `https://chat-app-tan-nine-10.vercel.app/api/messages/send/${selectedConversation._id}`,
+        `https://chat-app-backend-zhg0.onrender.com/api/messages/send/${selectedConversation._id}`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
